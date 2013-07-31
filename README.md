e1b8dc093d991a98654c1ed4753a8b88d979a63a
Johan Bleuzen <johan.bleuzen@gmail.com>
2013-07-31 06:41:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Pico Private Plugin
+
+Provide an authentication form to keep your site private.
+
+## Install
+
+1. Download a copy and extract the folder `pico_private` in your `plugins` folder,
+2. [Setup your theme](#setup_your_theme),
+3. Test everything is working with user "admin" and password "admin",
+4. Open the `pico_private_pass.php` file and insert your users and theirs password,
+5. **REMOVE admin user**.
+
+
+## How it works
+
+The whole site will be "protected" behind a login form.
+
+The plugin will display a login form when a user is not logged in.
+Once logged in, the user will be able to browse your website normally.
+
+Users and passwords are stored in `pico_private_pass.php` and you can add as many users you want.
+Passwords must be a SHA1 string.
+
+### Add users
+
+Users are stored in an associative array : 
+
+* Keys are usernames
+* Values are SHA1 passwords
+
+### Setup your theme
+
+All you need to do is create inside your theme's folder a `login.html` page which will display the login form. The form is pretty simple : 
+
+````twig
+<form method="post" action="">
+   {% if login_error %}<p class="error">{{ login_error }}</p>{% endif %}
+   <input type="text" name="username" id="username" placeholder="Username"/>
+   <input type="password" name="password" id="password" placeholder="Password"/>
+   <input class="alignright" type="submit" value="Login" />
+</form>
+```
+
+Then it's up to you to style the form to match your site design.
+
+When the user is logged_in, the plugin adds two variables to the `$twig_vars`, that you can use in your theme : 
+
+* `authed` : Is user logged_in (boolean)
+* `username` : The username (string)
---
+++ pico_private.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * A plugin that let you create a private Pico with authentication form
+ *
+ * @author Johan BLEUZEN
+ * @link http://www.johanbleuzen.fr
+ * @license http://opensource.org/licenses/MIT
+ */
+class Pico_Private {
+
+  private $theme;
+
+  public function __construct() {
+    $plugin_path = dirname(__FILE__);
+    session_start();
+
+    if(file_exists($plugin_path .'/pico_private_pass.php')){
+      global $pico_private_passwords;
+      include_once($plugin_path .'/pico_private_pass.php');
+      $this->passwords = $pico_private_passwords;
+    }
+  }
+
+  public function config_loaded(&$settings) {
+    $this->theme = $settings['theme'];
+  }
+
+  public function request_url(&$url) {
+    if($url == 'login') {
+      if($_SESSION['authed'] == false) {
+        return;
+      } else {
+        $this->redirect_home();
+        exit;
+      }
+    }
+
+    if($url == 'logout') {
+      session_destroy();
+      $this->redirect_login();
+    }
+    if(!isset($_SESSION['authed']) || $_SESSION['authed'] == false) {
+      $this->redirect_login();
+    }
+  }
+
+  public function before_render(&$twig_vars, &$twig) {
+    if(!isset($_SESSION['authed']) || $_SESSION['authed'] == false) {
+      // shortHand $_POST variables
+      $postUsername = $_POST['username'];
+      $postPassword = $_POST['password'];
+      if(isset($postUsername) && isset($postPassword)) {
+        if(isset($this->passwords[$postUsername]) == true && $this->passwords[$postUsername] == sha1($postPassword)) {
+          $_SESSION['authed'] = true;
+          $_SESSION['username'] = $postUsername;
+          $this->redirect_home();
+        } else {
+          $twig_vars['login_error'] = 'Invalid login';
+        }
+      }
+
+      header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+      $loader = new Twig_Loader_Filesystem(THEMES_DIR . $this->theme);
+      $twig_login = new Twig_Environment($loader, $twig_vars);   
+      $twig_vars['meta']['title'] = "Login";
+      echo $twig_login->render('login.html', $twig_vars);
+      exit;
+    }
+
+    $twig_vars['authed'] = $_SESSION['authed'];
+    $twig_vars['username'] =  $_SESSION['username'];
+  }
+
+  private function redirect_home() {
+    header('Location: /'); 
+    exit;
+  }
+
+  private function redirect_login() {
+    header('Location: /login'); 
+    exit;
+  }
+
+}
